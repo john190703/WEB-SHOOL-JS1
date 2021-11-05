63c28998ce048c806ba06df012dc202941d0e1df
Ivan Zhbanov <john190703@gmail.com>
2021-11-05 04:21:41 +0300

    completed release 1

--- release-1/index.js
+++ release-1/index.js
@@ -1,32 +1,52 @@
 //  'Nikita' -> 'Hi, Nikita'
  const hiFunction = () => {
-    //code here
+    let a='Anton'
+    return('Hi, '+a)
 }
-
+console.log(hiFunction())
 
 // 5, 6 -> 11
  const sum = () => {
-    //code here
+    let a=3
+    let b=12
+    return(a+b)
 }
-
+console.log(sum())
 
 //5 -> false
 //0 -> true
 //4 -> true
  const isEven = () => {
-    //code here
+    let a=6
+    if (a%2==0){
+        return('true')
+    }
+    else{
+        return('false')
+    }
 }
-
+console.log(isEven())
 
 // 2 -> i have 2 apples
  const apples = () => {
-    //code here
+    let a=1
+    if (a==0){
+        return('I have nothing')
+    }
+    if (a==1){
+        return('i have '+a+' apple')
+    }
+    else{
+        return('i have '+a+' apples')
+    }
 }
+console.log(apples())
 // 4 -> 16
  const getPower = () => {
-    //code here
+    let a=324
+    return a*a
 }
-
+console.log(getPower())
 module.exports = {
     hiFunction,
     sum,
